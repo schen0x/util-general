1fb03c3e341427a58493d1b575503cfb6c3acba9
schen0x <schen.git@gmail.com>
2021-06-02 20:06:07 +0900

    count line

---
+++ search_n_count_line_recursive.py
@@ -0,0 +1,22 @@
+import os
+
+def searchAndGetLineCount(path: list, ignorePath: str):
+    '''
+    Search and count total line number.
+
+    path = '../volatile'
+
+    searchStr = """
+       a() {
+           b
+       }
+    """
+
+    replaceStr = """
+       b() {
+           c
+       }
+    """
+
+    '''
+    pass
