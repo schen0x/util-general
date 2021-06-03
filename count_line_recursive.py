ec50e72b80e7c97fa0e7d24d2256cdea2fb65a73
schen0x <schen.git@gmail.com>
2021-06-03 13:51:48 +0900

    count_line feature: ignore pattern

--- count_line_recursive.py
+++ count_line_recursive.py
@@ -1,32 +1,40 @@
+# python3 only
 import os
 import re
-import fileinput
 
-def countLinesRecursive(path: str, ignorePathRegex: list):
+def countLinesRecursive(path: str, ignore: str):
     '''
     Search and count total line number.
     Example: countLinesRecursive("./", ["LICENSE", ".git"])
     Params:
         path = '../volatile'
-        ignorePathRegex = ["regex1", "regex2", ...]
+        ignore = """
+            .git
+            node_module
+        """
     '''
+    path = path.replace("/", os.sep)
+    path = path.replace("\\", os.sep)
 
-    s = path[-1]
-    if (s == os.path.sep) or (s == '/') or (s == '\\'):
-        path = path[0:-1]
-        return countLinesRecursive(path, ignorePathRegex)
-
-
-    for i in ignorePathRegex:
-        if re.search(i, path):
-            return 0
-
+    # dir
     if os.path.isdir(path):
         dir_line_counter = 0
+        s = path[-1]
+        if (s == os.path.sep) or (s == '/') or (s == '\\'):
+            path = path[0:-1]
+        path += os.sep
+
+        if _shouldIgnore(path, ignore):
+            return 0
+
         for fileName in os.listdir(path):
-            newPathStr = path + os.path.sep + fileName
-            dir_line_counter += countLinesRecursive(newPathStr, ignorePathRegex)
+            newPathStr = path + fileName
+            dir_line_counter += countLinesRecursive(newPathStr, ignore)
         return dir_line_counter
+
+    # file
+    if _shouldIgnore(path, ignore):
+        return 0
 
     return count_lines(path)
 
@@ -41,6 +49,28 @@ def count_lines(filePath: str):
     print(filePath + ": non-empty-lines " + str(lines_count))
     return lines_count
 
+def _shouldIgnore(path: str, ignore: str):
+    '''
+        return: True if should be ignored
+    '''
+    ignorePathRegex = ignore.split("\n")
+    for i in ignorePathRegex:
+        pattern = i.strip()
+        if (len(pattern) < 1):
+            continue
+        if re.search(pattern, path):
+            # if match, ignore
+            print(path)
+            print(pattern)
+            return True
+    return False
+    
+
 if __name__ == "__main__":
-    res = countLinesRecursive("./", ["LICENSE", ".git"])
+    ignore = """
+        \.git/
+        node_module/
+        LICENSE
+    """
+    res = countLinesRecursive("./", ignore)
     print("sum: " + str(res))
